8d42d658bf53edc757c2254c9a55b72baeb25ba6
DylanGates <Solomonafoh@gmail.com>
2023-12-18 08:37:12 +0000

    2nd Commit

--- index.js
+++ index.js
@@ -7,4 +7,11 @@ mongoose
     .connect("mongodb://localhost/HospitalDB", {useNewURLParser: true})
     .then(() => {
         console.log("Connected to Database.");
-    })
+    });
+    .catch((err) => {
+        console.log(err);
+    });
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000.');
+});
