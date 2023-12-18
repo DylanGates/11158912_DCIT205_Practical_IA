8a44da64b219e6cd9809a917ba3ca149b96d5b84
DylanGates <Solomonafoh@gmail.com>
2023-12-18 08:53:10 +0000

    3rd Commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const patientDetails = new mongoose.Schema ({
+    Surname: {
+        type: String,
+        required: true,
+    },
+    Othernames: {
+        type: String,
+        required: true,
+    },
+    relationshipWithPatient: {
+        type: String,
+        required: true,
+    },
+    age: {
+        type: Number,
+        required: true,
+    },
+    height: {
+        type: Number,
+        required: true,
+    },
+    weight: {
+        type: Number,
+        required: true,
+    },
+    gender: {
+        type: String,
+        required: true,
+    },
+    bloodGroup: {
+        type: String,
+        required: true,
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+    phoneNumber: {
+        type: Number,
+        required: true,
+    }
+});
--- index.js
+++ index.js
@@ -4,13 +4,18 @@ const mongoose = require('mongoose');
 const app = express();
 
 mongoose
-    .connect("mongodb://localhost/HospitalDB", {useNewURLParser: true})
+    .connect("mongodb://localhost/27017", {useNewURLParser: true})
     .then(() => {
-        console.log("Connected to Database.");
-    });
+        try {
+            console.log("Connected to Database.");
+        } catch (err) {
+            console.log(err);
+        }
+    })
     .catch((err) => {
         console.log(err);
     });
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000.');
